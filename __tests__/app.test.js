3512603043768ef9c3a580d122b63cc64fc51e26
Erik Ford <ekford18@gmail.com>
2020-06-08 12:59:49 -0700

    first test for app completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,15 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const createResponse = require('../lib/utils/createResponse');
 
 describe('createResponse', () => {
+  it('handle the / get route and return hi', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        res.text = 'hi';
+        expect(res.text).toEqual('hi');
+      });
+  });
 
 });
